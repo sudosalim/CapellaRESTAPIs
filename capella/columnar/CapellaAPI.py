d16faee02f9a654c70a81050b521634116cde157
Umang Agrawal <umang.agrawal@couchbase.com>
2024-02-22 14:34:12 +0530

    Streamlining Columnar nomenclature
    Change-Id: I7362dc81d5a70460c588dad7f84a11d194d4c32c
    Reviewed-on: https://review.couchbase.org/c/CapellaRESTAPIs/+/206165
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- capella/columnar/CapellaAPI.py
+++ capella/columnar/CapellaAPI.py
@@ -5,53 +5,57 @@ from ..common.CapellaAPI import CommonCapellaAPI
 
 class CapellaAPI(CommonCapellaAPI):
     """
-        GoldfishAPI class to support Goldfish / Serverless Analytics endpoints and operations.
+        Class to support Capella Columnar endpoints and operations.
 
         Parameters:
-            url (str): The base URL of the Goldfish API.
+            url (str): The base URL of the Columnar API.
             secret (str): The secret key for authentication.
             access (str): The access key for authentication.
             user (str): The username for authentication.
             pwd (str): The password for authentication.
-            TOKEN_FOR_INTERNAL_SUPPORT (str, optional): An optional token for internal support.
+            TOKEN_FOR_INTERNAL_SUPPORT (str, optional): An optional token
+            for internal support.
     """
 
-    def get_goldfish_services(self):
+    def get_columnar_services(self):
         """
-            Retrieve a list of all Goldfish services.
+            Retrieve a list of all Columnar services.
         """
-        url = "{}/internal/support/serverlessanalytics/service".format(self.internal_url)
+        url = "{}/internal/support/serverlessanalytics/service".format(
+            self.internal_url)
         resp = self.do_internal_request(url, "GET")
         return resp
 
-    def get_specific_goldfish_service(self, serviceId):
+    def get_specific_columnar_service(self, serviceId):
         """
-            Retrieve a specific Goldfish service.
+            Retrieve a specific Columnar service.
 
             Parameters:
                 serviceId : ID of the service whose service needs to be retrieved
         """
-        url = '{}/internal/support/serverlessanalytics/service/{}'.format(self.internal_url, serviceId)
+        url = '{}/internal/support/serverlessanalytics/service/{}'.format(
+            self.internal_url, serviceId)
         resp = self.do_internal_request(url, "GET")
         return resp
 
-    def create_goldfish_instance(self, tenant_id, project_id, name,
+    def create_columnar_instance(self, tenant_id, project_id, name,
                                  description, provider, region, nodes,
                                  **kwargs):
         """
-            Create a new Goldfish instance within a specified project.
+            Create a new Columnar instance within a specified project.
 
             Parameters:
                 tenant_id (str): The ID of the tenant under which the instance will be created.
                 project_id (str): The ID of the project where the instance will be added.
-                name (str): The name of the Goldfish instance.
-                description (str): A description for the Goldfish instance.
+                name (str): The name of the Columnar instance.
+                description (str): A description for the Columnar instance.
                 provider (str): The provider or cloud platform for the instance (e.g., AWS, Azure).
                 region (str): The region or location where the instance will be deployed.
                 nodes (int): The number of nodes to allocate for the instance.
                 **kwargs: Additional keyword arguments to pass to the API request.
         """
-        url = "{}/v2/organizations/{}/projects/{}/instance".format(self.internal_url, tenant_id, project_id)
+        url = "{}/v2/organizations/{}/projects/{}/instance".format(
+            self.internal_url, tenant_id, project_id)
         body = {
             "name": name,
             "description": description,
@@ -66,9 +70,10 @@ class CapellaAPI(CommonCapellaAPI):
                                         params=json.dumps(body))
         return resp
 
-    def get_goldfish_instances(self, tenant_id, project_id, page=1, perPage=100):
+    def get_columnar_instances(self, tenant_id, project_id, page=1,
+                               perPage=100):
         """
-            Retrieve a list of Goldfish instances within a specified project.
+            Retrieve a list of Columnar instances within a specified project.
 
             Parameters:
                 tenant_id (str): The ID of the tenant associated with the project.
@@ -77,55 +82,55 @@ class CapellaAPI(CommonCapellaAPI):
                 perPage (int): Number of results to return per page. Default is 100.
 
         """
-        url = "{}/v2/organizations/{}/projects/{}/instance?page={}&perPage={}".format(self.internal_url,
-                                                                                      tenant_id, project_id,
-                                                                                      page, perPage)
+        url = "{}/v2/organizations/{}/projects/{}/instance?page={}&perPage=" \
+              "{}".format(
+            self.internal_url, tenant_id, project_id, page, perPage)
         resp = self.do_internal_request(url, method="GET")
         return resp
 
-    def get_specific_goldfish_instance(self, tenant_id, project_id, instance_id):
+    def get_specific_columnar_instance(self, tenant_id, project_id, instance_id):
         """
-            Retrieve information about a specific Goldfish instance within a project.
+            Retrieve information about a specific Columnar instance within a project.
 
             Parameters:
                 tenant_id (str): The ID of the tenant associated with the project.
                 project_id (str): The ID of the project where the instance is located.
-                instance_id (str): The ID of the Goldfish instance to retrieve information for.
+                instance_id (str): The ID of the Columnar instance to retrieve information for.
         """
-        url = "{}/v2/organizations/{}/projects/{}/instance/{}".format(self.internal_url, tenant_id, project_id,
-                                                                      instance_id)
+        url = "{}/v2/organizations/{}/projects/{}/instance/{}".format(
+            self.internal_url, tenant_id, project_id, instance_id)
         resp = self.do_internal_request(url, method="GET")
         return resp
 
-    def delete_goldfish_instance(self, tenant_id, project_id, instance_id):
+    def delete_columnar_instance(self, tenant_id, project_id, instance_id):
         """
-            Delete a specific Goldfish instance within a project.
+            Delete a specific Columnar instance within a project.
 
             Parameters:
                 tenant_id (str): The ID of the tenant associated with the project.
                 project_id (str): The ID of the project where the instance is located.
-                instance_id (str): The ID of the Goldfish instance to delete.
+                instance_id (str): The ID of the Columnar instance to delete.
         """
-        url = "{}/v2/organizations/{}/projects/{}/instance/{}".format(self.internal_url, tenant_id, project_id,
-                                                                      instance_id)
+        url = "{}/v2/organizations/{}/projects/{}/instance/{}".format(
+            self.internal_url, tenant_id, project_id, instance_id)
         resp = self.do_internal_request(url, method="DELETE")
         return resp
 
-    def update_goldfish_instance(self, tenant_id, project_id, instance_id, name, description, nodes, **kwargs):
+    def update_columnar_instance(self, tenant_id, project_id, instance_id, name, description, nodes, **kwargs):
         """
-            Modify a specific Goldfish instance within a project.
+            Modify a specific Columnar instance within a project.
 
             Parameters:
                 tenant_id (str): The ID of the tenant associated with the project.
                 project_id (str): The ID of the project where the instance is located.
-                instance_id (str): The ID of the Goldfish instance to delete.
-                name (str): The name of the Goldfish instance.
-                description (str): A description for the Goldfish instance.
+                instance_id (str): The ID of the Columnar instance to delete.
+                name (str): The name of the Columnar instance.
+                description (str): A description for the Columnar instance.
                 nodes (int): The number of nodes to allocate for the instance.
                 **kwargs: Additional keyword arguments to pass to the API request.
         """
-        url = "{}/v2/organizations/{}/projects/{}/instance/{}".format(self.internal_url, tenant_id, project_id,
-                                                                      instance_id)
+        url = "{}/v2/organizations/{}/projects/{}/instance/{}".format(
+            self.internal_url, tenant_id, project_id, instance_id)
         body = {
             "name": name,
             "description": description,
@@ -138,29 +143,30 @@ class CapellaAPI(CommonCapellaAPI):
         return resp
 
     def create_api_keys(self, tenant_id, project_id, instance_id):
-        """"
-            Create an analytics apikey
+        """
+            Create a Columnar apikey
 
             Parameters:
                 tenant_id (str): The ID of the tenant associated with the project.
                 project_id (str): The ID of the project where the instance is located.
-                instance_id (str): The ID of the Goldfish instance to create keys for.
+                instance_id (str): The ID of the Columnar instance to create keys for.
         """
-        url = "{}/v2/organizations/{}/projects/{}/instance/{}/apikeys".format(self.internal_url, tenant_id,
-                                                                           project_id, instance_id)
+        url = "{}/v2/organizations/{}/projects/{}/instance/{}/apikeys".format(
+            self.internal_url, tenant_id, project_id, instance_id)
         resp = self.do_internal_request(url, method="POST")
         return resp
 
     def delete_api_keys(self, tenant_id, project_id, instance_id, api_key):
         """
-            Revoke an analytics apikey
+            Revoke a Columnar apikey
 
             tenant_id (str): The ID of the tenant associated with the project.
             project_id (str): The ID of the project where the instance is located.
-            instance_id (str): The ID of the Goldfish instance to create keys for.
+            instance_id (str): The ID of the Columnar instance to create keys for.
             api_key (str): api key id.
         """
         url = "{}/v2/organizations/{}/projects/{}/instance/{}/apikeys/{}".format(
             self.internal_url, tenant_id, project_id, instance_id, api_key)
-        resp = self.do_internal_request(url, method="DELETE", headers=self.basic_headers)
+        resp = self.do_internal_request(
+            url, method="DELETE", headers=self.basic_headers)
         return resp
