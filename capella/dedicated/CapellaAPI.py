e78813c8748c9bd44872e45e4ef85c045702607e
Shaazin Sheikh <shaazin.sheikh@couchbase.com>
2024-10-07 12:18:44 +0530

    CBQE-8276: 5/n Add support for internal token
    Change-Id: I513551d00305118896c1868f33e4cc173c975c68
    Reviewed-on: https://review.couchbase.org/c/CapellaRESTAPIs/+/217419
    Reviewed-by: shaazin19 <shaazin.sheikh@couchbase.com>
    Tested-by: shaazin19 <shaazin.sheikh@couchbase.com>
    Reviewed-by: <mohsin.ahmed@couchbase.com>

--- capella/dedicated/CapellaAPI.py
+++ capella/dedicated/CapellaAPI.py
@@ -1263,45 +1263,58 @@ class CapellaAPI(CommonCapellaAPI):
 
     def create_global_feature_flag(self, flag_name, payload):
         url = "{}/internal/support/features/flags/{}".format(self.internal_url, flag_name)
-        resp = self.do_internal_request(url, method="POST", params=json.dumps(payload))
+        resp = self._urllib_request(url, method="POST",
+                                    params=json.dumps(payload),
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def update_global_feature_flag(self, flag_name, payload):
         url = "{}/internal/support/features/flags/{}".format(self.internal_url, flag_name)
-        resp = self.do_internal_request(url, method="PUT", params=json.dumps(payload))
+        resp = self._urllib_request(url, method="PUT",
+                                    params=json.dumps(payload),
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def delete_global_feature_flag(self, flag_name):
         url = "{}/internal/support/features/flags/{}".format(self.internal_url, flag_name)
-        resp = self.do_internal_request(url, method="DELETE")
+        resp = self._urllib_request(url, method="DELETE",
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def list_tenant_feature_flags_internal(self, tenant_id):
         url = "{}/internal/support/features/{}/flags".format(self.internal_url, tenant_id)
-        resp = self.do_internal_request(url, method="GET")
+        resp = self._urllib_request(url, method="GET",
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def list_tenant_feature_flags_internal_specific(self, tenant_id, flag_name):
         url = "{}/internal/support/features/{}/flags?flags={}".format(self.internal_url, tenant_id, flag_name)
-        resp = self.do_internal_request(url, method="GET")
+        resp = self._urllib_request(url, method="GET",
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def create_tenant_feature_flag(self, tenant_id, flag_name, payload):
         url = "{}/internal/support/features/{}/flags/{}".format(self.internal_url, tenant_id, flag_name)
-        resp = self.do_internal_request(url, method="POST", params=json.dumps(payload))
+        resp = self._urllib_request(url, method="POST",
+                                    params=json.dumps(payload),
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def update_tenant_feature_flag(self, tenant_id, flag_name, payload):
         url = "{}/internal/support/features/{}/flags/{}".format(self.internal_url, tenant_id, flag_name)
-        resp = self.do_internal_request(url, method="PUT", params=json.dumps(payload))
+        resp = self._urllib_request(url, method="PUT",
+                                    params=json.dumps(payload),
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def delete_tenant_feature_flag(self, tenant_id, flag_name):
         url = "{}/internal/support/features/{}/flags/{}".format(self.internal_url, tenant_id, flag_name)
-        resp = self.do_internal_request(url, method="DELETE")
+        resp = self._urllib_request(url, method="DELETE",
+                                    headers=self.cbc_api_request_headers)
         return resp
 
     def initialize_feature_flags_from_launchdarkly(self):
         url = "{}/internal/support/features/flags/initialize".format(self.internal_url)
-        resp = self.do_internal_request(url, method="POST", params=json.dumps(payload))
+        resp = self._urllib_request(url, method="POST",
+                                    headers=self.cbc_api_request_headers)
         return resp
